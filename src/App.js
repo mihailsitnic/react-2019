3f0672fe760f4f55310b349b402faa2207aac412
Positron Solo <positronsolo@Positrons-MacBook-Air.local>
2019-01-01 17:39:13 +0200

    added react-select

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 import ArticleList from './components/article-list'
 import UserForm from './components/user-form'
+import Select from 'react-select'
 
 class App extends Component {
 
+    state = {
+        selected: null
+    }
+
+    handleSelect = selected => this.setState({ selected })
+
     render() {
+        const { articles } = this.props
+        const options = articles.map(article => ({
+            label: article.title,
+            value: article.id
+        }))
         return (
           <div>
               <UserForm />
+              <Select options = {options} value = {this.state.selected} onChange = {this.handleSelect} />
               <ArticleList articles = {this.props.articles} ref = {this.setListRef} />
           </div>
         );
